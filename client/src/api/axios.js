440a7f0d49ade3d9d33a6341acf16b104313b418
rahulkumarlexonit <rahulkumar@lexonit.com>
2026-02-14 11:45:55 +0530

    Updated API baseURL for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+//   baseURL: "http://localhost:5000/api",
+  baseURL :"https://expense-tracker-backend-rcnh.onrender.com/api",
 });
 
 // Attach token automatically
